e4ed6d9a2350cc2e2c670972ea9d9b0ca8a2303e
GRINE Alima <ea_grine@esi.dz>
2018-06-20 16:16:50 +0200

    get list movies now playing

--- app/src/main/java/com/example/slash/comflix/entities/Serie.kt
+++ app/src/main/java/com/example/slash/comflix/entities/Serie.kt
@@ -3,20 +3,5 @@ package com.example.slash.comflix.entities
 /**
  * Created by Slash on 13/04/2018.
  */
-class Serie {
-    var title:String?=null
-    var cover:Int?=null
-    var season:Int?=null
-    var episode:Int?=null
-    var serieId:Int?=null
-    var genre:String?=null
+data class Serie(val poster_path:String?, val popularity:Int, val id:Int, val backdrop:String?, val vote_average:Double,val overview:String)
 
-    constructor(title: String?, cover: Int?, season: Int?, episode: Int?,serieId:Int?,genre:String?) {
-        this.title = title
-        this.cover = cover
-        this.season = season
-        this.episode = episode
-        this.serieId=serieId
-        this.genre=genre
-    }
-}
